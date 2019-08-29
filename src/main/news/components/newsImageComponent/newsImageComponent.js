2cbfb2112abadf9ca5ad75cfbbbbee3d30912575
Lucija Tatarević <lucija.tatarevic@gmail.com>
2019-08-29 14:53:43 +0200

    Add activity indicator while image loads

--- src/main/news/components/newsImageComponent/newsImageComponent.js
+++ src/main/news/components/newsImageComponent/newsImageComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import { View, Animated, ActivityIndicator } from "react-native";
+import FastImage from "react-native-fast-image";
 
 import styles from "./newsImageStyles";
 
@@ -9,16 +10,53 @@ class NewsImage extends Component {
     this.state = { loading: true };
   }
 
+  handleThumbnailLoad = thumbnailAnimated => {
+    Animated.timing(thumbnailAnimated, {
+      toValue: 1
+    }).start();
+    this.loadEnd();
+  };
 
+  loadEnd = () => {
+    this.setState({ loading: false });
+  };
 
   render() {
     const { urlToImage } = this.props;
     const thumbnailImage =
       "https://images.unsplash.com/photo-1508612761958-e931d843bdd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80";
-   
+    let thumbnailAnimated = new Animated.Value(0);
 
     return (
-
+      <View style={styles.container}>
+        <Animated.Image
+          style={[
+            styles.image,
+            { opacity: this.thumbnailAnimated, backgroundColor: "#fff" }
+          ]}
+          source={{
+            uri: thumbnailImage
+          }}
+          resizeMode="cover"
+        />
+        <ActivityIndicator
+          style={[styles.activity, styles.image, styles.imageOverlay]}
+          animating={this.state.loading}
+        />
+        <FastImage
+          style={[
+            styles.imageOverlay,
+            { opacity: this.imageAnimated },
+            styles.image
+          ]}
+          source={{
+            uri: urlToImage
+          }}
+          resizeMode="cover"
+          onLoadEnd={this.loadEnd}
+          onError={e => this.handleThumbnailLoad(thumbnailAnimated)}
+        />
+      </View>
     );
   }
 }
